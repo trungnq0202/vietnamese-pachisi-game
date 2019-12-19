073f4d67c2518197e6e77f7cb4777baa7b8189f1
DaiHoang1411 <53077379+DaiHoang1411@users.noreply.github.com>
2019-12-19 18:16:39 +0700

    create menu.fxml

---
+++ src/controllers/MenuController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+public class MenuController {
+    private MainController mainController;
+    public MenuController() {
+
+    }
+
+    public void initialize() {
+
+    }
+    
+
+    public MainController getMainController() {
+        return mainController;
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+}
