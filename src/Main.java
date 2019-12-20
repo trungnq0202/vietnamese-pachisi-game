94e9f1c2862a038dd147ba3157e58e7285cc3b74
quangtrung <s3742774@rmit.edu.vn>
2019-12-21 00:50:19 +0700

    "Finish drawing game board"

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/view_fxml/board.fxml"));
+        primaryStage.setTitle("Co ca ngua");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
