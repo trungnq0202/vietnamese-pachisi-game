4db08b3b5d805a16106d56a3763dbbbd8bdd6f47
anhphu0n9 <pahn1303@gmail.com>
2019-12-23 01:27:08 +0700

    create startScreen and change Circle to Button(UI DESIGN)

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("resources/view_fxml/dice.fxml"));
-
-        primaryStage.setTitle("Dice Tester");
+        Parent root = FXMLLoader.load(getClass().getResource("resources/view_fxml/main.fxml"));
+        primaryStage.setTitle("UI Tester");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,6 +1,21 @@
 package controllers;
 
 public class MainController {
+    private StartSceneController startSceneController;
 
+//    private void initialize(){
+//        injectMainControllerInNestedControllers();
+//    }
 
+//    private void injectMainControllerInNestedControllers(){
+//        getStart().injectMainController(this);
+//    }
+
+    public StartSceneController getStart() {
+        return startSceneController;
+    }
+
+    public void setStartSceneController(StartSceneController startSceneController) {
+        this.startSceneController = startSceneController;
+    }
 }
